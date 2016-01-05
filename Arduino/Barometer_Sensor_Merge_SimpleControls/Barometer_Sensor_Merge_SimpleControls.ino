c381e5e06437be51527fd65b78ad0b6dd4e339ac
FredWe <robert.wei.176@gmail.com>
2016-01-05 15:17:39 +0100

    Add Arduino files

---
+++ Arduino/Barometer_Sensor_Merge_SimpleControls/Barometer_Sensor_Merge_SimpleControls.ino
@@ -0,0 +1,160 @@
+/*
+ * Barometer_Sensor_Merge_SimpleControls.ino
+ *
+ * Copyright (c) 2012 seeed technology inc.
+ * Website    : www.github.io/example
+ * Author     : Dong WEI, UPMC
+ * Create Time: 2015/12/23
+ * Change Log : Nothing Special
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+//"RBL_nRF8001.h/spi.h/boards.h" is needed in every new project
+#include <SPI.h>
+#include <EEPROM.h>
+#include <boards.h>
+#include <RBL_nRF8001.h>
+ 
+#define DIGITAL_OUT_PIN    A1
+#define DIGITAL_IN_PIN     A4
+#define ANALOG_IN_PIN      A0
+
+#include "Barometer.h"
+#include <Wire.h>
+
+float temperature;
+float pressure;
+float atm;
+float altitude;
+Barometer myBarometer;
+
+void setup() {
+  // put your setup code here, to run once:
+  // Enable serial debug
+  Serial.begin(57600);
+  myBarometer.init();
+
+  // Default pins set to 9 and 8 for REQN and RDYN
+  // Set your REQN and RDYN here before ble_begin() if you need
+  //ble_set_pins(3, 2);
+  
+  // Set your BLE advertising name here, max. length 10
+  ble_set_name("WrongBLE");
+  
+  // Init. and start BLE library.
+  ble_begin();
+  
+  pinMode(DIGITAL_OUT_PIN, OUTPUT);
+  pinMode(DIGITAL_IN_PIN, INPUT);
+  
+  // Default to internally pull high, change it if you need
+  digitalWrite(DIGITAL_IN_PIN, HIGH);
+  //digitalWrite(DIGITAL_IN_PIN, LOW);  
+}
+
+void loop() {
+  float pressure_ref_here = 102700;
+  temperature = myBarometer.bmp085GetTemperature(myBarometer.bmp085ReadUT()); //Get the temperature, bmp085ReadUT MUST be called first
+  pressure = myBarometer.bmp085GetPressure(myBarometer.bmp085ReadUP());//Get the temperature
+  altitude = myBarometer.calcAltitude(pressure, pressure_ref_here); //Uncompensated caculation - in Meters
+  atm = pressure / pressure_ref_here;
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature, 2); //display 2 decimal places
+  Serial.println(" deg C");
+
+  Serial.print("Pressure: ");
+  Serial.print(pressure, 0); //whole number only.
+  Serial.println(" Pa");
+
+  Serial.print("Ralated Atmosphere: ");
+  Serial.println(atm, 4); //display 4 decimal places
+
+  Serial.print("Altitude: ");
+  Serial.print(altitude, 2); //display 2 decimal places
+  Serial.println(" m");
+
+  Serial.println();
+  
+  // put your main code here, to run repeatedly:
+  static boolean analog_enabled = false;
+  
+  // If data is ready
+  while(ble_available())
+  {
+    // read out command and data
+    byte data0 = ble_read();
+    byte data1 = ble_read();
+    byte data2 = ble_read();
+    
+    if (data0 == 0x01)  // Command is to control digital out pin
+    {
+      if (data1 == 0x01)
+        digitalWrite(DIGITAL_OUT_PIN, HIGH);
+      else
+        digitalWrite(DIGITAL_OUT_PIN, LOW);
+    }
+    else if (data0 == 0xA0) // Command is to enable analog in reading
+    {
+      if (data1 == 0x01)
+        analog_enabled = true;
+      else
+        analog_enabled = false;
+    }
+    else if (data0 == 0x04)
+    {
+      analog_enabled = false;
+      digitalWrite(DIGITAL_OUT_PIN, LOW);
+    }
+  }
+  
+  if (analog_enabled)  // if analog reading enabled
+  {
+    digitalWrite(DIGITAL_OUT_PIN, HIGH);
+    // Read and send out
+    uint32_t value = uint32_t(temperature * 100); 
+    ble_write(0x0B);
+    
+    ble_write(value >> 8);
+    ble_write(value);
+    
+    value = uint32_t(pressure);
+    ble_write(value >> 16);
+    ble_write(value >> 8);
+    ble_write(value);
+
+    // Allow BLE Shield to send/receive data
+    ble_do_events();
+    digitalWrite(DIGITAL_OUT_PIN, LOW);
+  }
+  
+  if (!ble_connected())
+  {
+    analog_enabled = false;
+    digitalWrite(DIGITAL_OUT_PIN, LOW);
+  }
+  
+  // Allow BLE Shield to send/receive data
+  ble_do_events();  
+
+  delay(500); //wait a second and get values again.
+}
